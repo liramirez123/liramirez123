ebc0eb3be8a6d3e6ee75c0bde3550b403022e86c
Luis Ignacio Ramirez Martinez <liramirez@ayesa.es>
2021-04-03 09:52:20 -0500

    Se ha añadido un nuevo print

---
+++ fracion.py
@@ -0,0 +1,17 @@
+class Fraccion:
+
+    def __init__(self,arriba,abajo):
+
+        self.num = arriba
+        self.den = abajo
+
+    def mostrar(self):
+        print(self.num,"/",self.den)
+
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+
+
+miF = Fraccion(2,3)
+print(miF)
+print("Comí", miF , "de la pizza")
